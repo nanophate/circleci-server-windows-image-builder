1c9573ef64e1fdfcb96f46458f9fa7c32ba7477a
Naoya Matayoshi <nanophate@users.noreply.github.com>
2023-06-06 10:56:14 +0900

    Update configure-docker.ps1

--- windows/provision-scripts/configure-docker.ps1
+++ windows/provision-scripts/configure-docker.ps1
@@ -1,21 +1,14 @@
 $registryMirror = "https://mirror.gcr.io"
 
-# Check if Docker service is running
-$dockerService = Get-Service -Name Docker -ErrorAction SilentlyContinue
-if ($dockerService.Status -ne "Running") {
-    Write-Host "Docker service is not running."
-    exit
-}
-
 # Stop Docker service
 Stop-Service -Name Docker
 
 # Set the docker configutation
 $daemonConfigPath = "C:\ProgramData\Docker\config\daemon.json"
-$daemonConfig = Get-Content -Path $daemonConfigPath | ConvertFrom-Json
+$daemonConfig = @{}
 
 # Add or update registry mirror settings
-$daemonConfig.registry-mirrors = @($registryMirror)
+$daemonConfig.'registry-mirrors' = @($registryMirror)
 
 # Save the modified configuration file
 $daemonConfig | ConvertTo-Json | Set-Content -Path $daemonConfigPath
